5c0283856131fc7cb58f76570f88a1a8bf7b85fc
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-11-25 06:40:07 +0900

    feat: スクリプト注入をSPAにも対応させた

--- dist/sold_page_script.js
+++ dist/sold_page_script.js
@@ -12,59 +12,69 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 element.addEventListener('click', (e) => {
   chrome.runtime.sendMessage('gejelkpidobampgonfcdkkfgckaphban', item);
 }) */
-const product = {
-    images: [],
-    title: document
-        .querySelector('[data-testid="name"]')
-        .shadowRoot.querySelector('.heading.page').textContent,
-    price: Number(document.querySelector('[data-testid="price"]').getAttribute('value')),
-    shipping: document.querySelector('[title-label="商品の情報"]')
-        .nextElementSibling.children[2].lastChild.textContent,
-    text: document
-        .querySelector('[data-testid="description"]')
-        .shadowRoot.querySelector('slot')
-        .assignedNodes()[0].textContent,
-    category: document.querySelector('[title-label="商品の情報"]')
-        .nextElementSibling.children[0].lastChild.textContent,
-    condition: document.querySelector('[title-label:"商品の情報"]')
-        .nextElementSibling.children[1].lastChild.textContent,
-    shippingMethod: document.querySelector('[title-label="商品の情報"]')
-        .nextElementSibling.children[3].lastChild.textContent,
-    region: document.querySelector('[title-label="商品の情報"]')
-        .nextElementSibling.children[4].lastChild.textContent,
-    days: document.querySelector('[title-label="商品の情報"]')
-        .nextElementSibling.children[5].lastChild.textContent,
-};
-const createRelistButton = (element) => __awaiter(void 0, void 0, void 0, function* () {
-    return new Promise((resolve, reject) => __awaiter(void 0, void 0, void 0, function* () {
-        const divElement = document.createElement('div');
-        divElement.className = 'relist-button';
-        divElement.textContent = '再出品する！';
-        const targetElement = element.parentElement;
-        targetElement.appendChild(divElement);
-        resolve(_);
-    }));
-});
-const clickEvent = () => __awaiter(void 0, void 0, void 0, function* () {
-    const relistButtonElement = document.querySelector('div.relist-button');
-    relistButtonElement.addEventListener('click', () => {
-        console.log('test');
+function setProduct() {
+    const product = {
+        images: [],
+        title: document
+            .querySelector('[data-testid="name"]')
+            .shadowRoot.querySelector('.heading.page').textContent,
+        price: Number(document.querySelector('[data-testid="price"]').getAttribute('value')),
+        shipping: document.querySelector('[title-label="商品の情報"]')
+            .nextElementSibling.children[2].lastChild.textContent,
+        text: document
+            .querySelector('[data-testid="description"]')
+            .shadowRoot.querySelector('slot')
+            .assignedNodes()[0].textContent,
+        category: document.querySelector('[title-label="商品の情報"]')
+            .nextElementSibling.children[0].lastChild.textContent,
+        condition: document.querySelector('[title-label="商品の情報"]')
+            .nextElementSibling.children[1].lastChild.textContent,
+        shippingMethod: document.querySelector('[title-label="商品の情報"]')
+            .nextElementSibling.children[3].lastChild.textContent,
+        region: document.querySelector('[title-label="商品の情報"]')
+            .nextElementSibling.children[4].lastChild.textContent,
+        days: document.querySelector('[title-label="商品の情報"]')
+            .nextElementSibling.children[5].lastChild.textContent,
+    };
+    return product;
+}
+function createRelistButton(element) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            const divElement = document.createElement('div');
+            divElement.className = 'relist-button';
+            divElement.textContent = '再出品する！';
+            const targetElement = element.parentElement;
+            targetElement.appendChild(divElement);
+            resolve(_);
+        }));
     });
-});
+}
+function clickEvent() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const relistButtonElement = document.querySelector('div.relist-button');
+        relistButtonElement.addEventListener('click', () => {
+            console.log('test');
+            console.log(setProduct());
+        });
+    });
+}
 chrome.runtime.onMessage.addListener((value) => {
     alert(value);
 });
-const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    const interval = setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
-        console.log('繰り返し');
-        const element = document.querySelector('[data-testid="checkout-button-container"]');
-        if (element) {
-            clearInterval(interval);
-            yield createRelistButton(element);
-            yield clickEvent();
-        }
-    }), 1000);
-});
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const interval = setInterval(() => __awaiter(this, void 0, void 0, function* () {
+            console.log('繰り返し');
+            const element = document.querySelector('[data-testid="checkout-button-container"]');
+            if (element) {
+                clearInterval(interval);
+                yield createRelistButton(element);
+                yield clickEvent();
+            }
+        }), 1000);
+    });
+}
 main();
 function _(_) {
     throw new Error('Function not implemented.');
--- sold_page_script.ts
+++ sold_page_script.ts
@@ -16,34 +16,37 @@ element.addEventListener('click', (e) => {
   chrome.runtime.sendMessage('gejelkpidobampgonfcdkkfgckaphban', item);
 }) */
 
-const product: ProductInfo = {
-  images: [],
-  title: document
-    .querySelector('[data-testid="name"]')!
-    .shadowRoot!.querySelector('.heading.page')!.textContent!,
-  price: Number(
-    document.querySelector('[data-testid="price"]')!.getAttribute('value')
-  ),
-  shipping: document.querySelector('[title-label="商品の情報"]')!
-    .nextElementSibling!.children[2]!.lastChild!.textContent!,
-  text: document
-    .querySelector('[data-testid="description"]')!
-    .shadowRoot!.querySelector('slot')!
-    .assignedNodes()[0].textContent!,
-  category: document.querySelector('[title-label="商品の情報"]')!
-    .nextElementSibling!.children[0]!.lastChild!.textContent!,
-  condition: document.querySelector('[title-label:"商品の情報"]')!
-    .nextElementSibling!.children[1].lastChild!.textContent!,
-  shippingMethod: document.querySelector('[title-label="商品の情報"]')!
-    .nextElementSibling!.children[3].lastChild!.textContent!,
-  region: document.querySelector('[title-label="商品の情報"]')!
-    .nextElementSibling!.children[4].lastChild!.textContent!,
-  days: document.querySelector('[title-label="商品の情報"]')!
-    .nextElementSibling!.children[5].lastChild!.textContent!,
-};
+function setProduct() {
+  const product: ProductInfo = {
+    images: [],
+    title: document
+      .querySelector('[data-testid="name"]')!
+      .shadowRoot!.querySelector('.heading.page')!.textContent!,
+    price: Number(
+      document.querySelector('[data-testid="price"]')!.getAttribute('value')
+    ),
+    shipping: document.querySelector('[title-label="商品の情報"]')!
+      .nextElementSibling!.children[2]!.lastChild!.textContent!,
+    text: document
+      .querySelector('[data-testid="description"]')!
+      .shadowRoot!.querySelector('slot')!
+      .assignedNodes()[0].textContent!,
+    category: document.querySelector('[title-label="商品の情報"]')!
+      .nextElementSibling!.children[0]!.lastChild!.textContent!,
+    condition: document.querySelector('[title-label="商品の情報"]')!
+      .nextElementSibling!.children[1].lastChild!.textContent!,
+    shippingMethod: document.querySelector('[title-label="商品の情報"]')!
+      .nextElementSibling!.children[3].lastChild!.textContent!,
+    region: document.querySelector('[title-label="商品の情報"]')!
+      .nextElementSibling!.children[4].lastChild!.textContent!,
+    days: document.querySelector('[title-label="商品の情報"]')!
+      .nextElementSibling!.children[5].lastChild!.textContent!,
+  };
+  return product;
+}
 
-const createRelistButton = async (element: Element) =>
-  new Promise(async (resolve, reject) => {
+async function createRelistButton(element: Element) {
+  return new Promise(async (resolve, reject) => {
     const divElement = document.createElement('div');
     divElement.className = 'relist-button';
     divElement.textContent = '再出品する！';
@@ -53,19 +56,21 @@ const createRelistButton = async (element: Element) =>
 
     resolve(_);
   });
+}
 
-const clickEvent = async () => {
+async function clickEvent() {
   const relistButtonElement = document.querySelector('div.relist-button');
   relistButtonElement!.addEventListener('click', () => {
     console.log('test');
+    console.log(setProduct());
   });
-};
+}
 
 chrome.runtime.onMessage.addListener((value) => {
   alert(value);
 });
 
-const main = async () => {
+async function main() {
   const interval = setInterval(async () => {
     console.log('繰り返し');
     const element = document.querySelector(
@@ -77,7 +82,7 @@ const main = async () => {
       await clickEvent();
     }
   }, 1000);
-};
+}
 
 main();
 function _(_: any) {
