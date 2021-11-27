6a5a9d7559a8f01c6d7e0198cce121ccbe40687c
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-11-27 21:43:37 +0900

    feat: 再出品ボタンを押した際の簡易的な処理を実装した

--- background.ts
+++ background.ts
@@ -22,6 +22,35 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 });
 
-chrome.runtime.onMessage.addListener((result) => {
-  console.log(result);
+chrome.runtime.onMessage.addListener((productInfo) => {
+  //メルカリ出品ページを開く
+  chrome.tabs.create(
+    {
+      active: true,
+      url: 'https://jp.mercari.com/sell/create',
+    },
+    (tab) => {
+      //定期的に新しく開いたタブの情報を取得する
+      const interval = setInterval(() => {
+        //現在開いているタブの読み込み状態を取得する
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+          //読み込みが完了したら…
+          if (tabs[0].status === 'complete') {
+            clearInterval(interval);
+            //スクリプトファイルを注入する
+            chrome.scripting.executeScript({
+              target: { tabId: tab.id! },
+              files: ['dist/create_page_script.js'],
+            });
+          }
+          console.log(tabs);
+        });
+        console.log('新しいタブ繰り返し');
+      }, 1000);
+
+      console.log(tab);
+    }
+  );
+  // chrome.tabs.sendMessage();
+  console.log(productInfo);
 });
