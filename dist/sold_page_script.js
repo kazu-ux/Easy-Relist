714282d02d2a4a4328cea16de1fd8d04ae93fd40
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-12-01 17:44:01 +0900

    update: エラー処理を実装した

--- dist/sold_page_script.js
+++ dist/sold_page_script.js
@@ -13,6 +13,10 @@ function getImageUrl() {
     const imageElements = document.querySelectorAll('.slick-list [sticker]');
     for (const element of imageElements) {
         const imageUrl = element.getAttribute('src');
+        if (!imageUrl) {
+            alert('画像のURL取得に失敗しました');
+            return [''];
+        }
         imageUrls.push(imageUrl);
     }
     return imageUrls;
@@ -32,47 +36,57 @@ function getBase64(imageUrls) {
                 });
                 return reader.result;
             }));
-            imageBase64s.push(base64);
+            if (!base64) {
+                alert('画像のbase64取得にエラーが発生しました');
+                return [''];
+            }
+            imageBase64s.push(base64.toString());
         }
         return imageBase64s;
     });
 }
 function getCategories() {
     const categoryIds = [];
+    //pop() メソッドは、配列から最後の要素を取り除き、その要素を返します。
+    //このメソッドは配列の長さを変化させます。
     const targetElement = Array.from(document.querySelectorAll('mer-breadcrumb-list [data-location="item:item_detail_table:link:go_search"]')).pop();
+    if (!targetElement) {
+        alert('カテゴリー要素の取得に失敗しました');
+        return [];
+    }
     const tCategoryIds = targetElement.href.match(/t._category_id=[0-9]*/g);
+    if (!tCategoryIds) {
+        alert('カテゴリーID取得の正規表現にエラーが発生しました');
+        return [''];
+    }
     for (const tCategoryId of tCategoryIds) {
-        categoryIds.push(tCategoryId.match(/[0-9]+$/)[0]);
+        const formattedCategoryId = tCategoryId.match(/[0-9]+$/);
+        if (!formattedCategoryId) {
+            alert('カテゴリーIDの整形にエラーが発生しました');
+            return [''];
+        }
+        categoryIds.push(formattedCategoryId[0]);
     }
     console.log(categoryIds);
     return categoryIds;
 }
 function setProduct() {
-    var _a, _b;
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v;
     return __awaiter(this, void 0, void 0, function* () {
         const product = {
             images: yield getBase64(getImageUrl()),
             category: getCategories(),
-            size: (_a = document.querySelector('[data-testid="商品のサイズ"]')) === null || _a === void 0 ? void 0 : _a.textContent,
-            brand: (_b = document.querySelector('[data-testid="ブランド"]')) === null || _b === void 0 ? void 0 : _b.textContent,
-            itemCondition: document.querySelector('[data-testid="商品の状態"]')
-                .textContent,
-            name: document
-                .querySelector('[data-testid="name"]')
-                .getAttribute('title-label'),
-            description: document.querySelector('[data-testid="description"]')
-                .textContent,
-            shippingPayer: document.querySelector('[data-testid="配送料の負担"]')
-                .textContent,
-            shippingMethod: document.querySelector('[data-testid="配送の方法"]')
-                .textContent,
-            shippingFromArea: document.querySelector('[data-testid="発送元の地域"]')
-                .textContent,
-            shippingDuration: document.querySelector('[data-testid="発送までの日数"]')
-                .textContent,
-            price: document
-                .querySelector('[data-testid="price"]')
-                .getAttribute('value'),
+            size: (_b = (_a = document.querySelector('[data-testid="商品のサイズ"]')) === null || _a === void 0 ? void 0 : _a.textContent) !== null && _b !== void 0 ? _b : '',
+            brand: (_d = (_c = document.querySelector('[data-testid="ブランド"]')) === null || _c === void 0 ? void 0 : _c.textContent) !== null && _d !== void 0 ? _d : '',
+            itemCondition: (_f = (_e = document.querySelector('[data-testid="商品の状態"]')) === null || _e === void 0 ? void 0 : _e.textContent) !== null && _f !== void 0 ? _f : '',
+            name: (_h = (_g = document
+                .querySelector('[data-testid="name"]')) === null || _g === void 0 ? void 0 : _g.getAttribute('title-label')) !== null && _h !== void 0 ? _h : '',
+            description: (_k = (_j = document.querySelector('[data-testid="description"]')) === null || _j === void 0 ? void 0 : _j.textContent) !== null && _k !== void 0 ? _k : '',
+            shippingPayer: (_m = (_l = document.querySelector('[data-testid="配送料の負担"]')) === null || _l === void 0 ? void 0 : _l.textContent) !== null && _m !== void 0 ? _m : '',
+            shippingMethod: (_p = (_o = document.querySelector('[data-testid="配送の方法"]')) === null || _o === void 0 ? void 0 : _o.textContent) !== null && _p !== void 0 ? _p : '',
+            shippingFromArea: (_r = (_q = document.querySelector('[data-testid="発送元の地域"]')) === null || _q === void 0 ? void 0 : _q.textContent) !== null && _r !== void 0 ? _r : '',
+            shippingDuration: (_t = (_s = document.querySelector('[data-testid="発送までの日数"]')) === null || _s === void 0 ? void 0 : _s.textContent) !== null && _t !== void 0 ? _t : '',
+            price: (_v = (_u = document.querySelector('[data-testid="price"]')) === null || _u === void 0 ? void 0 : _u.getAttribute('value')) !== null && _v !== void 0 ? _v : '',
         };
         return product;
     });
--- sold_page_script.ts
+++ sold_page_script.ts
@@ -17,7 +17,11 @@ function getImageUrl(): string[] {
   let imageUrls: string[] = [];
   const imageElements = document.querySelectorAll('.slick-list [sticker]');
   for (const element of imageElements) {
-    const imageUrl = element.getAttribute('src')!;
+    const imageUrl = element.getAttribute('src');
+    if (!imageUrl) {
+      alert('画像のURL取得に失敗しました');
+      return [''];
+    }
     imageUrls.push(imageUrl);
   }
   return imageUrls;
@@ -37,21 +41,40 @@ async function getBase64(imageUrls: string[]) {
         });
         return reader.result;
       });
-    imageBase64s.push(base64 as string);
+    if (!base64) {
+      alert('画像のbase64取得にエラーが発生しました');
+      return [''];
+    }
+    imageBase64s.push(base64.toString());
   }
   return imageBase64s;
 }
 
 function getCategories() {
   const categoryIds: string[] = [];
+  //pop() メソッドは、配列から最後の要素を取り除き、その要素を返します。
+  //このメソッドは配列の長さを変化させます。
   const targetElement = Array.from(
     document.querySelectorAll(
       'mer-breadcrumb-list [data-location="item:item_detail_table:link:go_search"]'
     )
-  ).pop()! as HTMLLinkElement;
-  const tCategoryIds = targetElement.href.match(/t._category_id=[0-9]*/g)!;
+  ).pop() as HTMLLinkElement;
+  if (!targetElement) {
+    alert('カテゴリー要素の取得に失敗しました');
+    return [];
+  }
+  const tCategoryIds = targetElement.href.match(/t._category_id=[0-9]*/g);
+  if (!tCategoryIds) {
+    alert('カテゴリーID取得の正規表現にエラーが発生しました');
+    return [''];
+  }
   for (const tCategoryId of tCategoryIds) {
-    categoryIds.push(tCategoryId.match(/[0-9]+$/)![0]);
+    const formattedCategoryId = tCategoryId.match(/[0-9]+$/);
+    if (!formattedCategoryId) {
+      alert('カテゴリーIDの整形にエラーが発生しました');
+      return [''];
+    }
+    categoryIds.push(formattedCategoryId[0]);
   }
   console.log(categoryIds);
   return categoryIds;
@@ -59,28 +82,32 @@ function getCategories() {
 
 async function setProduct() {
   const product: ProductInfo = {
-    images: await getBase64(getImageUrl())!,
+    images: await getBase64(getImageUrl()),
     category: getCategories(),
-    size: document.querySelector('[data-testid="商品のサイズ"]')?.textContent!,
-    brand: document.querySelector('[data-testid="ブランド"]')?.textContent!,
-    itemCondition: document.querySelector('[data-testid="商品の状態"]')!
-      .textContent!,
-    name: document
-      .querySelector('[data-testid="name"]')!
-      .getAttribute('title-label')!,
-    description: document.querySelector('[data-testid="description"]')!
-      .textContent!,
-    shippingPayer: document.querySelector('[data-testid="配送料の負担"]')!
-      .textContent!,
-    shippingMethod: document.querySelector('[data-testid="配送の方法"]')!
-      .textContent!,
-    shippingFromArea: document.querySelector('[data-testid="発送元の地域"]')!
-      .textContent!,
-    shippingDuration: document.querySelector('[data-testid="発送までの日数"]')!
-      .textContent!,
-    price: document
-      .querySelector('[data-testid="price"]')!
-      .getAttribute('value')!,
+    size:
+      document.querySelector('[data-testid="商品のサイズ"]')?.textContent ?? '',
+    brand:
+      document.querySelector('[data-testid="ブランド"]')?.textContent ?? '',
+    itemCondition:
+      document.querySelector('[data-testid="商品の状態"]')?.textContent ?? '',
+    name:
+      document
+        .querySelector('[data-testid="name"]')
+        ?.getAttribute('title-label') ?? '',
+    description:
+      document.querySelector('[data-testid="description"]')?.textContent ?? '',
+    shippingPayer:
+      document.querySelector('[data-testid="配送料の負担"]')?.textContent ?? '',
+    shippingMethod:
+      document.querySelector('[data-testid="配送の方法"]')?.textContent ?? '',
+    shippingFromArea:
+      document.querySelector('[data-testid="発送元の地域"]')?.textContent ?? '',
+    shippingDuration:
+      document.querySelector('[data-testid="発送までの日数"]')?.textContent ??
+      '',
+    price:
+      document.querySelector('[data-testid="price"]')?.getAttribute('value') ??
+      '',
   };
   return product;
 }
