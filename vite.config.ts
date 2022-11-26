f133f670e20b0407df26cf20a52168db4170ea83
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2022-11-26 19:33:20 +0900

    update: permissionsを追加

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,13 @@ const manifest = defineManifest({
   name: 'らくらく再出品',
   version: '1.5',
   options_page: 'index.html',
-  permissions: ['webRequest'],
+  permissions: ['webRequest', 'scripting', 'tabs', 'storage'],
+
   background: { service_worker: 'src/service_worker.ts' },
   host_permissions: [
     'https://api.mercari.jp/items/get?id=*',
     'https://api.mercari.jp/draft_items/save',
+    'https://api.mercari.jp/draft_items/gets',
     'https://jp.mercari.com/transaction/*',
   ],
 });
