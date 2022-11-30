4c520126b4f69681a38fa76bba497a52f8a5f611
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2022-11-30 20:22:57 +0900

    update: 取引ページに再出品ボタンを設置する

--- src/ContentScripts/content_script.ts
+++ src/ContentScripts/content_script.ts
@@ -1,15 +1,18 @@
+import TransactionPage from './transaction_page';
+
 let href = location.href;
 
-console.log(href);
+TransactionPage();
 
 const observer = new MutationObserver(() => {
   if (href === location.href) return;
-  console.log(location.href);
 
-  const isSearchPage = location.href.includes('/tags/');
-  if (!isSearchPage) {
-    // chrome.runtime.sendMessage(WorksStyle.visible);
-  }
+  const isTransactionPage = location.href.includes(
+    'https://jp.mercari.com/transaction/'
+  );
+  if (!isTransactionPage) return;
+  TransactionPage();
+
   href = location.href;
 });
 
