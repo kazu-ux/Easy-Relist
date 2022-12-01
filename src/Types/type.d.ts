747bdef5325814ae6e9ddcb73c760547fe599ab8
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2022-12-01 20:10:10 +0900

    add: typeファイルを追加

---
+++ src/Types/type.d.ts
@@ -0,0 +1,14 @@
+type ItemData = {
+  images: string[];
+  category: string[];
+  size: string;
+  brand: string;
+  itemCondition: string;
+  name: string;
+  description: string;
+  shippingPayer: string;
+  shippingMethod: string;
+  shippingFromArea: string;
+  shippingDuration: string;
+  price: string;
+};
