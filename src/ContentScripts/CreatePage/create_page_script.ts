260680fa59b799dd9392da8732b795f03cb6ec20
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-03-02 11:21:32 +0900

    clean: リファクタリング

--- src/ContentScripts/CreatePage/create_page_script.ts
+++ src/ContentScripts/CreatePage/create_page_script.ts
@@ -33,51 +33,27 @@ function setBrand(brand: string) {
   targetElement.setAttribute('value', brand);
 }
 
-/* async function setAllCategory(soldCategories: string[]) {
-  function getCategoryList(index: number): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-      const categoryList: string[] = [];
-      const interval = setInterval(() => {
-        const targetElement: HTMLSelectElement | null = document.querySelector(
-          `[name="category${index}"] select`
-        );
-        if (targetElement) {
-          clearInterval(interval);
+const setCategory = (strings: string[]) => {
+  strings.unshift('カテゴリーを選択する');
 
-          const categoryOptions = targetElement.options;
-          for (const option of categoryOptions) {
-            categoryList.push(option.value);
-          }
+  let i = 0;
+  const links = document.getElementsByTagName('a');
 
-          resolve(categoryList);
-        } else {
-          alert('カテゴリー選択要素が見つかりません');
-        }
-        console.log(`getCategory${index}List 繰り返し`);
-      }, intervalTimes);
-    });
-  }
+  const clickLink = () => {
+    if (i < links.length) {
+      const link = links[i];
+      if (strings.includes(link.textContent ?? '')) {
+        link.click();
+        setTimeout(clickLink, 1000);
+      } else {
+        i++;
+        setTimeout(clickLink, 1000);
+      }
+    }
+  };
 
-  function judgeWhatNumber(categoryList: string[], categoryId: string) {
-    const index = categoryList.findIndex((target) => target === categoryId);
-    return index;
-  }
-
-  function setCategory(categoryListIndex: number, selectElementIndex: number) {
-    const targetElement =
-      document.querySelectorAll('select')[selectElementIndex];
-    targetElement.selectedIndex = categoryListIndex;
-    targetElement.dispatchEvent(new Event('change', { bubbles: true }));
-  }
-
-  await Promise.all(
-    soldCategories.map(async (categoryId, index) => {
-      const categoryList = getCategoryList(index + 1);
-      const categoryListIndex = judgeWhatNumber(await categoryList, categoryId);
-      setCategory(categoryListIndex, index);
-    })
-  );
-} */
+  clickLink();
+};
 
 function setItemInfoToSelect(itemObjForSelect: { [key: string]: string }) {
   const key = Object.keys(itemObjForSelect)[0];
@@ -111,46 +87,36 @@ function setItemInfoToSelect(itemObjForSelect: { [key: string]: string }) {
   });
 }
 
-function setItemName(itemText: { [key: string]: string }) {
-  const key = Object.keys(itemText)[0];
-  const value = itemText[key];
+const setItemName = async (itemText: { [key: string]: string }) => {
+  const [key, value] = Object.entries(itemText)[0];
   const targetElement: HTMLInputElement | null = document.querySelector(
     `input[name=${key}]`
   );
-  if (!targetElement) {
-    alert('商品名入力要素が見つかりません');
-    return;
-  }
+  if (!targetElement) throw new Error('商品名入力要素が見つかりません');
   targetElement.value = value;
   targetElement.dispatchEvent(new Event('input', { bubbles: true }));
-}
+};
 
-function setItemDiscription(description: string) {
+const setItemDescription = async (description: string) => {
   const targetElement: HTMLTextAreaElement | null = document.querySelector(
     'textarea[name="description"]'
   );
-  if (!targetElement) {
-    alert('説明文入力要素が見つかりません');
-    return;
-  }
+  if (!targetElement) throw new Error('説明文入力要素が見つかりません');
   targetElement.value = description;
   targetElement.dispatchEvent(new Event('input', { bubbles: true }));
-}
+};
 
-function setPrice(price: string) {
+const setPrice = async (price: string) => {
   const targetElement: HTMLInputElement | null = document.querySelector(
     'input[name="price"]'
   );
-  if (!targetElement) {
-    alert('価格入力要素が見つかりません');
-    return;
-  }
+  if (!targetElement) throw new Error('価格入力要素が見つかりません');
   targetElement.value = price;
   targetElement.dispatchEvent(new Event('input', { bubbles: true }));
-}
+};
 
 async function setToAllItems(productInfo: ItemData) {
-  imageUpload(productInfo.images);
+  await imageUpload(productInfo.images);
   // await setAllCategory(productInfo.category);
   if (productInfo.size) {
     setItemInfoToSelect({ size: productInfo.size });
@@ -161,15 +127,15 @@ async function setToAllItems(productInfo: ItemData) {
     }, intervalTimes);
   }
 
-  setItemName({ name: productInfo.name });
-  setItemDiscription(productInfo.description);
-  setItemInfoToSelect({ itemCondition: productInfo.itemCondition });
+  await setItemName({ name: productInfo.name });
+  await setItemDescription(productInfo.description);
+  await setItemInfoToSelect({ itemCondition: productInfo.itemCondition });
 
+  await setPrice(productInfo.price);
   await setItemInfoToSelect({ shippingPayer: productInfo.shippingPayer });
-  setItemInfoToSelect({ shippingMethod: productInfo.shippingMethod });
-  setItemInfoToSelect({ shippingFromArea: productInfo.shippingFromArea });
-  setItemInfoToSelect({ shippingDuration: productInfo.shippingDuration });
-  setPrice(productInfo.price);
+  // setItemInfoToSelect({ shippingMethod: productInfo.shippingMethod });
+  await setItemInfoToSelect({ shippingFromArea: productInfo.shippingFromArea });
+  await setItemInfoToSelect({ shippingDuration: productInfo.shippingDuration });
   return;
 }
 
@@ -238,8 +204,9 @@ const setup = async () => {
   if (!itemData) return;
 
   await setToAllItems(itemData);
+  setCategory;
   // setShippingMethod(itemData.shippingMethod);
-  ChromeStorage.deleteItemData();
+  // ChromeStorage.deleteItemData();
   await ChromeStorage.setIsLoading(false);
 };
 
