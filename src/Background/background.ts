e5f1ae75dfd7182992e3099715f52b44cda2ad2e
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-04-04 09:58:08 +0900

    update: メルカリAPIへのアクセスを追加

--- src/Background/background.ts
+++ src/Background/background.ts
@@ -1,26 +1,43 @@
+import ChromeStorage from '../Storage/chrome_storage';
+
 type Data = { name: string; value: string };
 
 chrome.action.onClicked.addListener(async () => {
+  console.log('アイコンクリック');
+
   const headers = new Headers();
   const data: Data[] = (await chrome.storage.local.get('headers')).headers;
   data.forEach(({ name, value }) => {
     headers.append(name, value);
   });
-  for (const [key, value] of headers) {
-    console.log(`${key} = ${value}`);
-  }
 
-  /*   fetch(
+  fetch(
     ' https://api.mercari.jp/items/get?id=m65099380065&include_item_attributes=true',
     { headers }
   )
     .then((response) => response.json())
     .then((data) => console.log(data))
-    .catch((error) => console.log(error)); */
+    .catch((error) => console.log(error));
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeinfo, tab) => {
-  // console.log(tabId, changeinfo, tab);
+chrome.runtime.onMessage.addListener(async (message) => {
+  console.log(message);
+  const headers = new Headers();
+  const data: Data[] = (await chrome.storage.local.get('headers')).headers;
+  data.forEach(({ name, value }) => {
+    headers.append(name, value);
+  });
+  /*     for (const [key, value] of headers) {
+      // console.log(`${key} = ${value}`);
+    } */
+  const itemId = await ChromeStorage.getItemId();
+  if (!itemId) return;
+  const url = `https://api.mercari.jp/items/get?id=${itemId}`;
+
+  fetch(url, { headers })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
 });
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
