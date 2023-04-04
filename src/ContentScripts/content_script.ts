5b636fbdde7bfdfe881529eceda72654c2b917dc
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2023-04-04 09:59:20 +0900

    update: 再出品ボタンを設置する処理を追加

--- src/ContentScripts/content_script.ts
+++ src/ContentScripts/content_script.ts
@@ -1,21 +1,57 @@
-import TransactionPage from './TransactionPage/transaction_page';
+import ChromeStorage from '../Storage/chrome_storage';
 
-const isTransactionPage = (url: string) => {
-  return url.includes('https://jp.mercari.com/transaction/');
-};
+(async () => {
+  const relistButtonName = 'relist_button';
 
-let href = location.href;
-if (isTransactionPage(href)) {
-  TransactionPage();
-}
+  const relistButton = () => {
+    const handleClick = async () => {
+      const currentURL = window.location.href;
+      const itemId = currentURL.split('/')[4];
+      console.log(itemId);
 
-const observer = new MutationObserver(() => {
-  if (href === location.href) return;
+      const itemPageURL = 'https://jp.mercari.com/item/' + itemId;
 
-  if (!isTransactionPage(location.href)) return;
-  TransactionPage();
+      await ChromeStorage.setIsLoading(true);
+      window.open(itemPageURL, '_blank');
+    };
 
-  href = location.href;
-});
+    const button = document.createElement('button');
+    button.innerText = 'Relist';
+    button.classList.add(relistButtonName);
+    button.onclick = handleClick;
+    return button;
+  };
 
-observer.observe(document, { childList: true, subtree: true });
+  // debounce関数の定義
+  const debounce = (func: () => void, wait: number) => {
+    let timeout: number | undefined;
+    return () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(func, wait);
+    };
+  };
+
+  // MutationObserverのインスタンス作成
+  const observer = new MutationObserver(
+    debounce(async () => {
+      // DOM変更が検出されたときの処理
+      const url = window.location.href;
+      if (!url.includes('https://jp.mercari.com/transaction/')) {
+        document.querySelector(`.${relistButtonName}`)?.remove();
+        return;
+      }
+      if (document.querySelector(`.${relistButtonName}`)) return;
+      document.querySelector('body')?.before(relistButton());
+      console.log(url);
+    }, 1000)
+  );
+
+  // 監視対象の要素を取得
+  const target = document.querySelector('body')!;
+
+  // 監視オプションを設定
+  const config = { childList: true, subtree: true, attributes: true };
+
+  // 監視開始
+  observer.observe(target, config);
+})();
